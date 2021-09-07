1b431e7e7fca577ebcf758cf79495fdf0a66726d
shivamraj6288 <shivamraj.study@gmail.com>
2021-09-07 23:43:18 +0530

    submission folder added

--- 190050113-190020010-190050017/code/queue.py
+++ 190050113-190020010-190050017/code/queue.py
@@ -1,6 +1,8 @@
 import heapq
 
+#stores the evnt queue
 eventq = []
 
+#function to add event in the queue
 def pushq(event):
     heapq.heappush(eventq, (event.time, event))
--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-ency(i,j,m):
-#     lat = p[i.nid][j.nid]
-#     if i.speed == 1 and j.speed == 1:
-#         lat = m/100
-
