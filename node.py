43d535a46e4442039bef04be999a46ad3ab60b5e
shivamraj6288 <shivamraj.study@gmail.com>
2021-09-07 18:07:05 +0530

    block verification , adding recieved block done

--- node.py
+++ node.py
@@ -12,22 +12,17 @@ class Node:
     peer=set()
     txnReceived=set()
 
-    def __init__(self, nid, speed, genesis):
+    def __init__(self, nid, speed, genesis, miningTime):
         self.nid=nid
-        self.speed=speed # 0=fast, 1=slow
+        self.speed=speed # 1=fast, 0=slow
         self.lbid=genesis.bid
         self.blockChain[genesis.bid]=copy.deepcopy(genesis)
+        self.miningTime=miningTime
 
-    def addBlock(self,block):
+    def addPeer(self,peerId):
+        self.peer.add(peerId)
 
-        temp=copy.deepcopy(block)
-        # temp.length=self.blockChain[temp.pbid].length+1
-        self.blockChain[temp.bid]=temp
-        if temp.length>self.blockChain[self.lbid].length:
-            self.lbid=temp.bid
-
-      
-    def txnSend(self, event, hp,lat):
+    def txnSend(self, event, hp):
         self.txnReceived.add(event.txn.tid);
 
         for a in self.peers:
@@ -37,14 +32,48 @@ class Node:
             
 
 
-    def txnRecv(self,event,hp, lat):
+    def txnRecv(self,event,hp):
         if event.txn.tid in self.txnReceived:
             return 
-        
+        self.txnReceived.add(event.txn.tid)
+
         for a in self.peers:
             t=event.time+lat
             action=TxnRecv(time=t,sender=self.nid,receiver=a,txn=event.txn)
             heapq.heappush(hp,(t,action))
+    
+    def blockVerify(self,cblock):
+        pblock=self.blockChain[cblock.pbid]
+        for a in cblock.txnIncluded:
+            sb=pblock.balance[a.sender]-a.value
+            if sb<0:
+                return False
+            if sb!=cblock.balance[a.sender]:
+                return False
+        return True
+
+    def mineNewBlock(self,block):
+        remaingTxn=self.txnReceived.difference(block.txnPool)
+
+    def addReceivedBlock(self,block):
+
+        if self.blockVerify(block)!=True:
+            return False
+
+        temp=copy.deepcopy(block)
+        # temp.length=self.blockChain[temp.pbid].length+1
+        self.blockChain[temp.bid]=temp
+        if temp.length>self.blockChain[self.lbid].length:
+            self.lbid=temp.bid
+
+        return True
+    
+    
+    
+
+      
+
+
 
     
     
