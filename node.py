748dbf3679b69db7dde29d0cef7f756d86ee3b76
shivamraj6288 <shivamraj.study@gmail.com>
2021-09-07 19:29:54 +0530

    node.py done

--- node.py
+++ node.py
@@ -3,7 +3,7 @@ import copy
 from numpy.core.numeric import tensordot 
 from block import Block
 from transaction import Transaction
-from event import TxnRecv, BlockMined
+from event import *
 import heapq
 import numpy as np 
 from random import sample
@@ -18,6 +18,7 @@ class Node:
     balance=0
     peer=set()
     txnReceived=set()
+    blockReceived=set()
 
 
     def __init__(self, nid, speed, genesis, miningTime):
@@ -30,24 +31,24 @@ class Node:
     def addPeer(self,node):
         self.peer.add(node)
 
-    def txnSend(self, event, hp):
+    def txnSend(self, event):
         self.txnReceived.add(event);
 
-        for a in self.peers:
-            t=event.time+computeLatency(self,a)
+        for a in self.peer:
+            t=event.time+computeLatency(self,a,1)
             action=TxnRecv(time=t,sender=self,receiver=a,txn=event.txn)
             pushq(action)
 
             
 
 
-    def txnRecv(self,event,hp):
+    def txnRecv(self,event):
         if event.txn in self.txnReceived:
             return 
         self.txnReceived.add(event.txn)
 
-        for a in self.peers:
-            t=event.time+computeLatency(self,a)
+        for a in self.peer:
+            t=event.time+computeLatency(self,a,1)
             action=TxnRecv(time=t,sender=self.nid,receiver=a,txn=event.txn)
             pushq(action)
     
@@ -69,9 +70,9 @@ class Node:
                 return False
         return True
 
-    def mineNewBlock(self,block,hp,lat):
+    def mineNewBlock(self,block,lat):
         remaingTxn=self.txnReceived.difference(block.txnPool)
-        numTxn=np.random.randint(0,len(remaingTxn))
+        numTxn=max(np.random.randint(0,len(remaingTxn)),900)
 
         txnToInclude=set(sample(remaingTxn,numTxn))
         txnId=np.random.randint(0,2**128-1)
@@ -95,43 +96,37 @@ class Node:
 
 
 
-    def verifyAndAddReceivedBlock(self,event,hp):
+    def verifyAndAddReceivedBlock(self,event):
 
         if self.blockVerify(event.block)!=True:
             return False
+        
+        
 
         temp=copy.deepcopy(event.block)
         # temp.length=self.blockChain[temp.pbid].length+1
         self.blockChain[temp.bid]=temp
         if temp.length>self.blockChain[self.lbid].length:
             self.lbid=temp.bid
-            self.mineNewBlock(block=temp,hp=hp,lat=event.time)
+            self.mineNewBlock(block=temp,lat=event.time)
 
+        
+        for a in self.peer:
+            lat=computeLatency(i=self,j=a,m=100+len(temp.txnIncluded))
+            lat=event.time+lat
+            action=BlockRecv(time=lat,sender=self,receiver=a,block=temp)
+            pushq(action)
 
         return True
     
-    def reciveMinedBlock(self, event, hp):
+    def receiveSelfMinedBlock(self, event):
         temp=copy.deepcopy(event.block)
         self.blockChain[temp.bid]=temp
 
         if temp.length>self.blockChain[self.lbid].length:
-            
-
-    
-    
-        
-    
-    
-    
-    
-
-      
-
-
-
-    
-    
-
-
-
-        
+            self.lbid=temp.bid
+            for a in self.peer:
+                lat=computeLatency(i=self,j=a,m=100+len(temp.txnIncluded))
+                lat=event.time+lat
+                action=BlockRecv(time=lat,sender=self,receiver=a,block=temp)
+                pushq(action)    
