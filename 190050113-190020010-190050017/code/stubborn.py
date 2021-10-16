d82442ce3c5d7e7b07a51afe7d4dfa6186907fbc
shivamraj6288 <shivamraj.study@gmail.com>
2021-10-17 00:57:23 +0530

    bug fixed

--- 190050113-190020010-190050017/code/stubborn.py
+++ 190050113-190020010-190050017/code/stubborn.py
@@ -5,3 +5,30 @@ class StubBlockchain(AdvBlockchain):
     def __init__(self, gblock):
         super().__init__(gblock)
     
+    def add_others_block(self, block, time):
+        self.blocks[block.bid]=(block,time)
+
+        if block.length > self.head.length :
+            self.head=block 
+
+            old_state=self.state
+
+            if self.state==1:
+                self.state=-1
+                self.private_lead=0 
+            elif self.state==0:
+                self.private_head=block 
+                self.private_lead=0
+                self.state=0 
+            elif self.state==2:
+                self.state=0
+                self.private_lead=0 
+                self.head=self.private_head
+            
+            else :
+                self.state-=1
+                self.private_head-=1
+            
+            return old_state
+
+        return -100 
